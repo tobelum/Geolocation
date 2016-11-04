594d71db870739d7826198488f0c8951aea34f82
tobelum <tohbelle@gmail.com>
2016-11-04 11:06:45 +0000

    another correction

--- js/index.js
+++ js/index.js
@@ -15,31 +15,31 @@
 			});
 		}
 
-	document.getElementById("scanner").onclick=function barcode(){
-	cordova.plugins.barcodeScanner.scan(onResult, errorMessage,
-      {
-          "preferFrontCamera" : true, // iOS and Android 
-          "showFlipCameraButton" : true, // iOS and Android 
-          "prompt" : "Place a barcode inside the scan area", // supported on Android only 
-          "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED 
-          "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device 
-      }
-   	);
-	}
+	// document.getElementById("scanner").onclick=function barcode(){
+	// cordova.plugins.barcodeScanner.scan(onResult, errorMessage,
+ //      {
+ //          "preferFrontCamera" : true, // iOS and Android 
+ //          "showFlipCameraButton" : true, // iOS and Android 
+ //          "prompt" : "Place a barcode inside the scan area", // supported on Android only 
+ //          "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED 
+ //          "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device 
+ //      }
+ //   	);
+	// }
 
-	document.getElementById("geolocation").addEventListener("click", function(){
-		navigator.geolocation.getCurrentPosition(onSuccess,onError, 
-	{enableHighAccuracy:true});
+	// document.getElementById("geolocation").addEventListener("click", function(){
+	// 	navigator.geolocation.getCurrentPosition(onSuccess,onError, 
+	// {enableHighAccuracy:true});
 
-	});
-	//watch position
-	var watchID = navigator.geolocation.watchPosition(onWatchSuccess, onWatchError,
-		{timeout : 30000}
-		);
+	// });
+	// //watch position
+	// var watchID = navigator.geolocation.watchPosition(onWatchSuccess, onWatchError,
+	// 	{timeout : 30000}
+	// 	);
 
-	document.getElementById("clearWatchbtn").addEventListener("click", function() {
-		navigator.geolocation.clearWatch(watchID);
-	});
+	// document.getElementById("clearWatchbtn").addEventListener("click", function() {
+	// 	navigator.geolocation.clearWatch(watchID);
+	// });
 	
 };
 
@@ -55,37 +55,37 @@
 	}
 
 
-	//barcode bit
-    function onResult(result) {
-        alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-    }
+	// //barcode bit
+ //    function onResult(result) {
+ //        alert("We got a barcode\n" +
+ //                "Result: " + result.text + "\n" +
+ //                "Format: " + result.format + "\n" +
+ //                "Cancelled: " + result.cancelled);
+ //    }
 
-    function errorMessage(error) {
-        alert("Scanning failed: " + error);
-    }
+ //    function errorMessage(error) {
+ //        alert("Scanning failed: " + error);
+ //    }
 
-    //geolocation bit
-    function onSuccess(position){
-		alert('Latitude: ' + position.coords.latitude + '\n' +
-							'Longitude: ' + position.coords.longitude + '\n');
-	}
-	function onError(error){
-		alert('code: ' + error.code + '\n' +
-				'message: ' + error.message + '\n');
-	}
+ //    //geolocation bit
+ //    function onSuccess(position){
+	// 	alert('Latitude: ' + position.coords.latitude + '\n' +
+	// 						'Longitude: ' + position.coords.longitude + '\n');
+	// }
+	// function onError(error){
+	// 	alert('code: ' + error.code + '\n' +
+	// 			'message: ' + error.message + '\n');
+	// }
 
-	//watch position bit
-	var onWatchSuccess = function(position) {
-		var element = document.getElementById('divWatchMeMove');
-		element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' + 'Longitude: ' + position.coords.longitude + '<br />' + '<hr />' + element.innerHTML;
-	};
+	// //watch position bit
+	// var onWatchSuccess = function(position) {
+	// 	var element = document.getElementById('divWatchMeMove');
+	// 	element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' + 'Longitude: ' + position.coords.longitude + '<br />' + '<hr />' + element.innerHTML;
+	// };
 
-	function onWatchError(error) {
-		alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
-	}
+	// function onWatchError(error) {
+	// 	alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+	// }
 
 })();
 
