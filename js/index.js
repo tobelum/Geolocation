7c7b77c8c5a068bcbca8b9359615ad0444de1735
tobelum <tohbelle@gmail.com>
2016-11-06 21:40:04 +0000

    debugged the camera, barcode and geolocation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+/*document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+function onDeviceReady(){
+		alert("here");
+		//pictureSource = navigator.camera.PictureSourceType;
+		//destinationType = navigator.camera.destinationType;
+
+		document.getElementById("capturePhoto").onclick = function(){
+			navigator.camera.getPicture(onPhotoDataSuccess, onFail,{
+				quality : 50,
+				destinationType:destinationType.DATA_URL
+			});
+}
+}*/
 
 (function(){
 
@@ -5,9 +18,10 @@
 	var pictureSource;
 	var destinationType;
 	function onDeviceReady(){
+		//alert("here");
 		pictureSource = navigator.camera.PictureSourceType;
 		destinationType = navigator.camera.destinationType;
-
+}
 		document.getElementById("capturePhoto").onclick = function(){
 			navigator.camera.getPicture(onPhotoDataSuccess, onFail,{
 				quality : 50,
@@ -15,33 +29,33 @@
 			});
 		}
 
-	// document.getElementById("scanner").onclick=function barcode(){
-	// cordova.plugins.barcodeScanner.scan(onResult, errorMessage,
- //      {
- //          "preferFrontCamera" : true, // iOS and Android 
- //          "showFlipCameraButton" : true, // iOS and Android 
- //          "prompt" : "Place a barcode inside the scan area", // supported on Android only 
- //          "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED 
- //          "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device 
- //      }
- //   	);
-	// }
+	document.getElementById("scanner").onclick=function barcode(){
+	cordova.plugins.barcodeScanner.scan(onResult, errorMessage,
+      {
+          "preferFrontCamera" : true, // iOS and Android 
+          "showFlipCameraButton" : true, // iOS and Android 
+          "prompt" : "Place a barcode inside the scan area", // supported on Android only 
+          "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED 
+          "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device 
+      }
+   	);
+	}
+	var watchID;
+	document.getElementById("geolocation").addEventListener("click", function(){
+//		navigator.geolocation.getCurrentPosition(onSuccess,onError, 
+//	{enableHighAccuracy:true});
+	//watch position
+	watchID = navigator.geolocation.watchPosition(onWatchSuccess, onWatchError,
+		{timeout : 30000}
+		);
 
-	// document.getElementById("geolocation").addEventListener("click", function(){
-	// 	navigator.geolocation.getCurrentPosition(onSuccess,onError, 
-	// {enableHighAccuracy:true});
+	});
 
-	// });
-	// //watch position
-	// var watchID = navigator.geolocation.watchPosition(onWatchSuccess, onWatchError,
-	// 	{timeout : 30000}
-	// 	);
-
-	// document.getElementById("clearWatchbtn").addEventListener("click", function() {
-	// 	navigator.geolocation.clearWatch(watchID);
-	// });
+	document.getElementById("clearWatchbtn").addEventListener("click", function() {
+		var element1 = document.getElementById('divWatchMeMove');
+		element1.innerHTML="";
+	});
 	
-};
 
 	//camera bit
 	function onPhotoDataSuccess(imageData){
@@ -55,19 +69,19 @@
 	}
 
 
-	// //barcode bit
- //    function onResult(result) {
- //        alert("We got a barcode\n" +
- //                "Result: " + result.text + "\n" +
- //                "Format: " + result.format + "\n" +
- //                "Cancelled: " + result.cancelled);
- //    }
+	//barcode bit
+    function onResult(result) {
+        alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+    }
 
- //    function errorMessage(error) {
- //        alert("Scanning failed: " + error);
- //    }
+    function errorMessage(error) {
+        alert("Scanning failed: " + error);
+    }
 
- //    //geolocation bit
+    //geolocation bit
  //    function onSuccess(position){
 	// 	alert('Latitude: ' + position.coords.latitude + '\n' +
 	// 						'Longitude: ' + position.coords.longitude + '\n');
@@ -77,15 +91,15 @@
 	// 			'message: ' + error.message + '\n');
 	// }
 
-	// //watch position bit
-	// var onWatchSuccess = function(position) {
-	// 	var element = document.getElementById('divWatchMeMove');
-	// 	element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' + 'Longitude: ' + position.coords.longitude + '<br />' + '<hr />' + element.innerHTML;
-	// };
+	//watch position bit
+	var onWatchSuccess = function(position) {
+		var element = document.getElementById('divWatchMeMove');
+		element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' + 'Longitude: ' + position.coords.longitude + '<br />' + '<hr />' + element.innerHTML;
+	};
 
-	// function onWatchError(error) {
-	// 	alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
-	// }
+	function onWatchError(error) {
+		alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+	}
 
 })();
 
